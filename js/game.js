74e0f4dd3a0f702ec7a7df4f329bb93cb9d86251
catteine <catteine@zuminternet.com>
2019-12-04 10:42:05 +0900

    카드 맞추기 게임 - 리팩토링 중

--- js/game.js
+++ js/game.js
@@ -22,7 +22,7 @@ Game.isSelecting = false;
 Game.selectedCard = [];
 
 Game.Card = function(val,el){
-  this.icon = val;
+  this.value = val;
   this.element = el;
 };
 
@@ -42,42 +42,43 @@ Game.changeTurn = function(){
 Game.matchCard = function(){
   const that = this,
   val_1 = that.selectedCard[0],
-  val_2 = that.selectedCard[1];
+  val_2 = that.selectedCard[1],
+  val_1_El = val_1.element,
+  val_2_El = val_2.element;
 
-  setTimeout(function(){
-    if (val_1.icon === val_2.icon) {
-      let score = that.players.A.score;
-      score = score + 1;
-      that.players.A.score = score;
+  setTimeout(() => {
+    if (val_1.value === val_2.value) {
+      val_1_El.classList.add('cleared');
+      val_2_El.classList.add('cleared');
+      
+      let thisPlayer = that.is1PTurn ? that.players.A : that.players.B;
+      let score = thisPlayer.score + 1;
+      thisPlayer.score = score;
       document.querySelector('.this_turn .score').innerHTML = score;
 
-      val_1.element.classList.add('cleared');
-      val_2.element.classList.add('cleared');
-      
-        if (that.is1PTurn) {
-          val_1.element.classList.add('win_a');
-          val_2.element.classList.add('win_a');
-        } else {
-          val_1.element.classList.add('win_b');
-          val_2.element.classList.add('win_b');
-        }
+      if (that.is1PTurn) {
+        val_1_El.classList.add('win_a');
+        val_2_El.classList.add('win_a');
+      } else {
+        val_1_El.classList.add('win_b');
+        val_2_El.classList.add('win_b');
+      }
 
       if (document.querySelectorAll('.cleared').length === that.maxScore) {
-        setTimeout(function(){
-          let player = 'A';
-          if (!that.is1PTurn) {
-            player = 'B'
-          }
+        setTimeout(() => {
+          let player = that.is1PTurn ? 'A' : 'B';
           alert(player + "의 승리!");
         }, 500);
       }
     } else {
-      val_1.element.classList.remove('reversed');
-      val_2.element.classList.remove('reversed');
-      that.changeTurn();
+      val_1_El.classList.remove('reversed');
+      val_2_El.classList.remove('reversed');
+      setTimeout(() => {
+        that.changeTurn();
+      }, 500);
     }
     that.selectedCard = [];
-    setTimeout(function(){
+    setTimeout(() => {
       that.isSelecting = false;
     }, 500);
   }, 500);
@@ -88,10 +89,8 @@ Game.makeCard = function(val){
 
   const cardNode = document.createElement('div');
   cardNode.className = "card";
-
   const icon = document.createElement('span');
   icon.className = "icon icon_" + val;
-  
   cardNode.appendChild(icon);
   that.Stage.appendChild(cardNode);
 
@@ -104,7 +103,7 @@ Game.makeCard = function(val){
         that.isSelecting = true;
         thisCard.classList.add('reversed');
         that.selectedCard.push(card);
-        setTimeout(function(){
+        setTimeout(() => {
           if (that.selectedCard.length === 2) {
             that.matchCard();
           } else {
